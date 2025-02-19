ca5c190c8c779c54ad4be91b48bd329d3abb91d3
LawakBADUT <146788952+lawakBADUT@users.noreply.github.com>
2025-02-19 10:27:40 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -72,6 +72,25 @@ $routes->group('admin', ['filter' => 'auth'], function ($routes) {
     $routes->post('manage_akun/update/(:num)', 'Manage_akunController::update/$1', ['filter' => 'role:admin']);
     $routes->post('manage_akun/delete/(:num)', 'Manage_akunController::delete/$1', ['filter' => 'role:admin']);
 
+    // Routes untuk manage kelas
+    // Manage kelas (daftar kelas & tambah kelas)
+    $routes->get('manage_kelas', 'Manage_kelasController::index', ['filter' => 'role:admin']);
+    $routes->get('manage_kelas/tambah', 'Manage_kelasController::tambah', ['filter' => 'role:admin']);
+    $routes->post('manage_kelas/simpan', 'Manage_kelasController::simpan', ['filter' => 'role:admin']);
+    $routes->get('manage_kelas/edit/(:num)', 'Manage_kelasController::edit/$1', ['filter' => 'role:admin']);
+    $routes->post('manage_kelas/update/(:num)', 'Manage_kelasController::update/$1', ['filter' => 'role:admin']);
+    $routes->post('manage_kelas/delete/(:num)', 'Manage_kelasController::delete/$1', ['filter' => 'role:admin']);
+    //  Manage kelas (kelola anggota)
+    $routes->get('manage_kelas/kelola_anggota', 'Manage_kelasController::kelola_anggota', ['filter' => 'role:admin']);
+    $routes->get('manage_kelas/kelola_anggota/detail/(:num)', 'Manage_kelasController::detail/$1', ['filter' => 'role:admin']);
+    $routes->get('manage_kelas/kelola_anggota/tambah/(:num)', 'Manage_kelasController::tambah_anggota/$id', ['filter' => 'role:admin']);
+    $routes->post('manage_kelas/kelola_anggota/simpan', 'Manage_kelasController::simpan_anggota', ['filter' => 'role:admin']);
+    $routes->post('manage_kelas/kelola_anggota/hapus/(:num)', 'Manage_kelasController::hapus_anggota/$1', ['filter' => 'role:admin']);
+    //  Manage kelas (evaluasi pembelajaran)
+    $routes->get('manage_kelas/evaluasi', 'Manage_kelasController::evaluasi', ['filter' => 'role:admin']);
+    $routes->post('manage_kelas/evaluasi/update', 'Manage_kelasController::update_evaluasi', ['filter' => 'role:admin']);
+
+
     // Routes untuk manage artikel
     $routes->get('artikel', 'ArtikelController::index', ['filter' => 'role:admin']); // Menampilkan daftar artikel
     $routes->get('artikel/tambah', 'ArtikelController::tambah', ['filter' => 'role:admin']); // Menampilkan form tambah
