fd8b30052d4d09726b1264d6b43b4f0abccf046b
LawakBADUT <146788952+lawakBADUT@users.noreply.github.com>
2025-02-28 14:51:35 +0700

    Dashboard admin (tinggal yang kelas)

--- app/Views/admin/templates/dashboard.php
+++ app/Views/admin/templates/dashboard.php
@@ -10,7 +10,7 @@
   <meta name="author" content="">
 
   <title>Robonesia | Dashboard</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 
   <link href="<?= base_url(); ?>/css/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link
@@ -315,8 +315,7 @@
       sparkline3.render();
     </script>
     <!--end::Script-->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 
