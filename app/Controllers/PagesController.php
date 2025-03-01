866fad6579767ef744a029d37bd02db9c9997d88
t-sofiachairani <tsofiachairani@mhs.unimed.ac.id>
2025-03-01 16:43:51 +0700

    shop

--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -81,4 +81,10 @@ class PagesController extends BaseController
 
         return $this->response->setJSON($timData);
     }
+
+    public function shop()
+    {
+        $data = ['title' => 'Robonesia | Shop',];
+        $this->renderView('pages/shop', $data);
+    }
 }
---
+++ app/Views/pages/shop.php
@@ -0,0 +1,22 @@
+<section class="shop">
+  <div class="shop__container">
+    <header>
+      <h1>Shopping</h1>
+      <div class="shopping">
+        <i class="fa-solid fa-bag-shopping"></i>
+        <span class="quantity">0</span>
+      </div>
+    </header>
+
+    <div class="list"></div>
+  </div>
+
+  <div class="card">
+    <h1>Card</h1>
+    <ul class="listCard"></ul>
+    <div class="checkOut">
+      <div class="total">0</div>
+      <div class="closeShopping">Close</div>
+    </div>
+  </div>
+</section>
