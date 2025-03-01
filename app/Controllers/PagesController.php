a1dc608cfa267fa4b63a49251aab44d0653ea1e1
LawakBADUT <146788952+lawakBADUT@users.noreply.github.com>
2025-03-01 17:30:17 +0700

    Reapply "shop"
    This reverts commit 32a2ea00642a63dca58b539fabf80df20bc3db17.

--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -83,4 +83,10 @@ class PagesController extends BaseController
 
         return $this->response->setJSON($timData);
     }
+
+    public function shop()
+    {
+        $data = ['title' => 'Robonesia | Shop',];
+        $this->renderView('pages/shop', $data);
+    }
 }
